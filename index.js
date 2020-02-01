ca2032802e7e95956e9a3cb6f5054de9ffd57f6c
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-02-01 15:29:47 -0800

    starter code

---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+"use strict";
+
+var inquirer = require("inquirer");
+var chalk = require("chalk");
+
+var response = chalk.blue;
+
+var resume = require("./resume.json");
+
+var resumePrompts = {
+  type: "list",
+  name: "resumeOptions",
+  message: "What would you like to know about me?",
+  choices: [...Object.keys(resume), "Exit"]
+};
+
+function main() {
+  console.log("Hello, my name is Marine. Welcome to my resume!");
+  resumeHandler();
+}
+
+function resumeHandler() {
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == "Exit") {
+      return;
+    }
+    var option = answer.resumeOptions;
+    console.log(response("<><><><><><><><><><><><><><><><><><>"));
+    resume[`${option}`].forEach(info => {
+      console.log(response("|   => " + info));
+    });
+    console.log(response("<><><><><><><><><><><><><><><><><><>"));
+    // console.log(resume[`${option}`]);
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.exitBack == "Back") {
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+}
+
+main();
